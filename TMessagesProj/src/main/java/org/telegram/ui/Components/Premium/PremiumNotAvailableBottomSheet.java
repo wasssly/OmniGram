0651fb2545a3b1ab7375f3cae0d782863259ffdf
wasssly <190352023+wasssly@users.noreply.github.com>
2026-09-21 17:52:24 +0000

    Update OmniGram runtime identifiers

--- TMessagesProj/src/main/java/org/telegram/ui/Components/Premium/PremiumNotAvailableBottomSheet.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/Premium/PremiumNotAvailableBottomSheet.java
@@ -52,7 +52,7 @@ public class PremiumNotAvailableBottomSheet extends BottomSheet {
         buttonTextView.setText(LocaleController.getString(R.string.InstallOfficialApp));
         buttonTextView.setOnClickListener(v -> {
             try {
-                v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=org.telegram.messenger")));
+                v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.omnigram.app")));
             } catch (ActivityNotFoundException e) {
                 FileLog.e(e);
             }
